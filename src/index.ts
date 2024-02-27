7ef00e14b321c3446075a82a9dad816c8d996024
summermong <2514o_o@naver.com>
2024-02-27 15:22:40 +0900

    #5.7 chain

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,61 @@
-import { init, exit } from './myPackage';
+import crypto from 'crypto';
 
-init({ debug: false, url: 'true' });
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash('sha256').update(toHash).digest('hex');
+  }
+}
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) {
+      return '';
+    } else {
+      return this.blocks[this.blocks.length - 1].hash;
+    }
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new Blockchain();
+blockchain.addBlock('First one');
+blockchain.addBlock('Second one');
+blockchain.addBlock('Third one');
+blockchain.addBlock('Fourth one');
+
+blockchain.getBlocks().push(new Block('hacked', 12345678, 'HACKED!'));
+// 해킹해서 가짜 블록을 추가해도 추가되지 않는다!
+
+console.log(blockchain.getBlocks());
